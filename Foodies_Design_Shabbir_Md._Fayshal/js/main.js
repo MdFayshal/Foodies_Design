b9d1c164a055e26699ab62125720114ccf835dbb
MdFayshal <shabbir.mdfayshal01@gmail.com>
2021-11-11 15:20:44 +0600

    Signed-off-by: MdFayshal <shabbir.mdfayshal01@gmail.com>

--- Foodies_Design_Shabbir_Md._Fayshal/js/main.js
+++
@@ -1,9 +0,0 @@
-
-$('document').ready(function(){
-//Active Menu Highlight
-$(document).on('click','ul li a',function(){
-  $('ul li a').removeClass('active');
-  $(this).addClass('active');
-})
-
-})
